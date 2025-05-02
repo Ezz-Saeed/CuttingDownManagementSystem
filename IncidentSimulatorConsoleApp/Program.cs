d4dab492acadd6108ca367838bc2efc6e0707eb5
Ezz Saeed <ezzsaid299@gmail.com>
2025-05-02 20:05:18 +0300

    Implementing IncidentGeneratorSimulator in console app to call the APIs concurentlly and forever

---
+++ IncidentSimulatorConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace IncidentSimulatorConsoleApp
+{
+    using IncidentSimulatorConsoleApp.Services;
+    using System.Net.Http.Json;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var simulator = new IncidentGeneratorSimulator(4,10);
+            await simulator.CallGenerateAPIsAsync();
+
+            //const string baseUrl = "http://localhost:5288/api/Incidents";
+            //HttpClient httpClient = new();
+
+            //int parallelRequests = 5;
+            //int delayBetweenRoundsInSeconds = 10;
+
+            //Console.WriteLine("Incident Generator Started...");
+
+            //while (true)
+            //{
+            //    var tasks = new List<Task>();
+
+            //    await Parallel.ForEachAsync(Enumerable.Range(0, parallelRequests), async (i, _) =>
+            //    {
+            //        try
+            //        {
+            //            var random = new Random();
+            //            var count = random.Next(5, 20);
+            //            var closedPercentage = random.Next(30, 80);
+
+            //            if (i % 2 == 0)
+            //            {
+            //                var response = await httpClient.PostAsync($"{baseUrl}/generateIncidentsA?count={count}&closedPercentage={closedPercentage}", null);
+            //                Console.WriteLine($"[A] Sent {count} incidents. Status: {response.StatusCode}");
+            //            }
+            //            else
+            //            {
+            //                var response = await httpClient.PostAsync($"{baseUrl}/generateIncidentsB?count={count}&closedPercentage={closedPercentage}", null);
+            //                Console.WriteLine($"[B] Sent {count} incidents. Status: {response.StatusCode}");
+            //            }
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine($"Error: {ex.Message}");
+            //        }
+            //    });
+
+            //    await Task.Delay(delayBetweenRoundsInSeconds * 1000);
+            //}
+        }
+    }
+
+}
---
+++ IncidentSimulatorConsoleApp/Services/IncidentGEneratorSimulator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentSimulatorConsoleApp.Services
+{
+    public class IncidentGeneratorSimulator(int parallelRequests = 5, int delayBetweenRoundsInSeconds = 10)
+    {
+        private const string baseUrl = "http://localhost:5288/api/Incidents";
+        private HttpClient httpClient = new();
+        private static  readonly ThreadLocal<Random> random = new(() => new Random());
+
+        //private readonly int parallelRequests = 5;
+        //private int delayBetweenRoundsInSeconds = 10;
+
+        public async Task  CallGenerateAPIsAsync()
+        {
+            Console.WriteLine("Incident Generator Started...");
+
+            while (true)
+            {
+                var tasks = new List<Task>();
+
+                await Parallel.ForEachAsync(Enumerable.Range(0, parallelRequests), async (i, _) =>
+                {
+                    try
+                    {
+                        //var random = new Random();
+                        var count = random.Value?.Next(5, 20);
+                        var closedPercentage = random.Value?.Next(30, 80);
+
+                        if (i % 2 == 0)
+                        {
+                            var response = await httpClient.
+                            PostAsync($"{baseUrl}/generateIncidentsA?count={count}&closedPercentage={closedPercentage}", null);
+                            Console.WriteLine($"[A] Sent {count} incidents. Status: {response.StatusCode}");
+                        }
+                        else
+                        {
+                            var response = await httpClient.
+                            PostAsync($"{baseUrl}/generateIncidentsB?count={count}&closedPercentage={closedPercentage}", null);
+                            Console.WriteLine($"[B] Sent {count} incidents. Status: {response.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                });
+
+                await Task.Delay(delayBetweenRoundsInSeconds * 1000);
+            }
+        }
+    }
+}
