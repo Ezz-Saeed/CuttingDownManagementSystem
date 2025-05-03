a899f89b3b76d6193c1a7bd5c426a216e42e845a
Ezz Saeed <ezzsaid299@gmail.com>
2025-05-03 16:16:50 +0300

    Calling The Sp_Create SP from console app

--- IncidentSimulatorConsoleApp/Program.cs
+++ IncidentSimulatorConsoleApp/Program.cs
@@ -8,9 +8,16 @@
         static async Task Main(string[] args)
         {
             var simulator = new IncidentGeneratorSimulator();
-            await simulator.CallGenerateAPIsAsync();
+            SPCreateCaller spCaller = new();
 
-            
+
+            var cts = new CancellationTokenSource();
+            var simulatorTask = Task.Run(() => simulator.CallGenerateAPIsAsync());
+            var spCreateTask = Task.Run(() => spCaller.RunAsync(cts.Token));
+
+            await Task.WhenAll(simulatorTask, spCreateTask);
+
+
         }
     }
 
---
+++ IncidentSimulatorConsoleApp/Services/SPCreateCaller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentSimulatorConsoleApp.Services
+{
+    public class SPCreateCaller
+    {
+        private readonly string localConnection = 
+                "Server=DESKTOP-2SR000U\\SQLEXPRESS01;Database=CutDownDb;Trusted_Connection=true;Encrypt=false";
+
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Exec_SP_Create();
+                await Task.Delay(5000, cancellationToken);
+            }
+        }
+
+        public void Exec_SP_Create()
+        {
+            using SqlConnection connection = new SqlConnection(localConnection);
+            using SqlCommand command = new SqlCommand("SP_Create", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Stored procedure executed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
